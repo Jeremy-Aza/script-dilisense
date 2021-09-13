b21cf530a186734c101b9552a41e8a7d10180982
Jeremy <bvbjerencript_09@hotmail.com>
2021-09-13 11:27:50 -0500

    Changes in data reception

--- dilisense_data_script.py
+++ dilisense_data_script.py
@@ -1,14 +1,17 @@
 import pandas as pd
-from pandas.io import json
 import requests
+import inquirer
+import getpass
+import openpyxl
+import sys
+import re
 
 # reader = pd.read_excel("C:/Users/User/Desktop/test_dilisense.xlsx", "Hoja1")
-reader = pd.read_excel(
-    "C:/Users/User/Desktop/test_dilisense.xlsx", "Hoja2")
+#reader = pd.read_excel("C:/Users/User/Desktop/test_dilisense.xlsx", "Hoja2")
 
 # data = reader.head()
 
-name = dict()
+name = list()
 data = []
 
 
@@ -43,45 +46,45 @@ def api_url(name):
         pep_type_title = "Tipo de PEP"
         name_found_title = "Coincidencias de nombre"
         source_id_title = "Fuente"
-        other_information_title = "Otra Información"
         links_title = "Links"
 
-        # print(jsonData["found_records"][0])
+        # print(jsonData["found_records"])
 
         if(len(jsonData["found_records"]) > 0):
-            # print(jsonData["found_records"][0]["sanction_details"])
             found_records = jsonData["found_records"][0]
             gender = found_records["gender"]
             date_of_birth = found_records["date_of_birth"]
             citizenship = found_records["citizenship"]
             source_type = found_records["source_type"]
             given_names = found_records["given_names"]
-            description = jsonData["found_records"][1]["description"]
-            occupations = jsonData["found_records"][1]["occupations"]
-            place_of_birth = jsonData["found_records"][1]["place_of_birth"]
-            pep_type = jsonData["found_records"][1]["pep_type"]
+            description = found_records["description"]
+            occupations = found_records["occupations"]
+            place_of_birth = found_records["place_of_birth"]
+            pep_type = found_records["pep_type"]
             entity_type = found_records["entity_type"]
             name_found = found_records["name"]
             source_id = found_records["source_id"]
-            links = found_records["links"]
+
+        if(len(jsonData["found_records"]) > 1):
+            links = jsonData["found_records"][0]["links"]
 
         data.append(
             {name_title: name[index],
              timestamp_title: jsonData["timestamp"],
              total_hits_title: jsonData["total_hits"],
              gender_title: gender if len(jsonData["found_records"]) > 0 else no_data,
-             date_of_birth_title: date_of_birth[0] if len(jsonData["found_records"]) > 0 else no_data,
+             date_of_birth_title: date_of_birth if len(jsonData["found_records"]) > 0 else no_data,
              citizenship_title: citizenship if len(jsonData["found_records"]) > 0 else no_data,
              source_type_title: source_type if len(jsonData["found_records"]) > 0 else no_data,
              given_names_title: given_names if len(jsonData["found_records"]) > 0 else no_data,
              description_title: description if len(jsonData["found_records"]) > 0 else no_data,
-             occupations_title: occupations if occupations else no_data if len(jsonData["found_records"]) > 0 else no_data,
+             occupations_title: occupations if len(jsonData["found_records"]) > 0 else no_data,
              place_of_birth_title: place_of_birth if len(jsonData["found_records"]) > 0 else no_data,
              entity_type_title: entity_type if len(jsonData["found_records"]) > 0 else no_data,
              pep_type_title: pep_type if len(jsonData["found_records"]) > 0 else no_data,
              name_found_title: name_found if len(jsonData["found_records"]) > 0 else no_data,
              source_id_title: source_id if len(jsonData["found_records"]) > 0 else no_data,
-             links_title: links if len(jsonData["found_records"]) > 0 else no_data,
+             links_title: links if len(jsonData["found_records"]) > 1 else no_data,
              })
 
     # Create DataFrame
@@ -91,17 +94,49 @@ def api_url(name):
     print(df)
 
     # Save excel
-    df.to_excel('testing.xlsx', sheet_name='example')
+    route_user = 'C:/Users/'+getpass.getuser() + '/Desktop/Resultados.xlsx'
+    df.to_excel(route_user, sheet_name='Resultados_inidividuales')
+    print("Se ha guardado satisfactoriamente su documento en la siguiente dirección:", route_user)
 
 
 # print(reader["Name"])
 
+# Introduce the path file
+def validate_input_path(value):
+    regex_path = '[a-zA-Z]:[\\\/](?:[a-zA-Z0-9\-\_\.\¿\'\¡\{\}\[\]\,\!\+\´\$\#\%\(\)\&\=]+[\\\/])*([a-zA-Z0-9\-\_\.\¿\'\¡\{\}\[\]\,\!\+\´\$\#\%\(\)\&\=]+\.xlsx)'
+    resultado = re.search(regex_path, value)
+    return resultado
+
+
+validate = False
+
+print('Ingrese la ruta del archivo (e.j.: C:/Users/User/Desktop/name_file.xlsx): ', end='')
+
+# Until a correct route is entered
+while not validate:
+    path_file = input()
+    validate = validate_input_path(path_file)
+    if not validate:
+        print('Lo sentimos, debemos introducir una ruta válida: ', end='')
+
+# Select the sheet in the file excel
+xl = pd.ExcelFile(path_file)
+xl.sheet_names
+questions = [
+    inquirer.List('sheetDocs',
+                  message="Eliga la hoja del documento para obtener los datos",
+                  choices=xl.sheet_names,
+                  ),
+]
+chooice_sheet = inquirer.prompt(questions)
+
+# Read the excel file
+reader = pd.read_excel(path_file, chooice_sheet["sheetDocs"])
+# print(reader)
+
+# Data user
 for tags, contenido in reader.items():
-    if(tags == "Name" or tags == "Nombre"):
+    if(tags == "Name" or tags == "Nombre" or tags == "name" or tags == "nombre" or tags == "Full Name" or tags == "Nombre Completo"):
         # print(contenido.to_dict())
         # Call API
         api_url(contenido.to_list())
-
-
-""" print(list(reader.get("Name").values))
-print(list(reader.get("Gender").values)) """
